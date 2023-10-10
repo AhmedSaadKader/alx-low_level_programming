965cf22b1310cfab9ca81c02479c37371660a10e
ahmed saad <ahmed.saad.kader@gmail.com>
2023-10-10 16:05:54 -0700

    11- fix bug

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,13 +10,13 @@ void print_to_98(int n)
 {
 if (n <= 98)
 {
-	for (i = n; i <= 98; i++)
+	while( n > 98)
 		printf("%d, ", n++); 
 	printf("%d\n", n);
 }
 else
 {
-	for (i = n; i >= 98; i--)
+	while(n < 98)
 		printf("%d, ", n--);
 	printf("%d\n", n);
 }
