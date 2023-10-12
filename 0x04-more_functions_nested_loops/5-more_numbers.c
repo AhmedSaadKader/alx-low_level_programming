e4d1147df0e735857aea3ad320a6c2f873b14c02
ahmed saad <ahmed.saad.kader@gmail.com>
2023-10-12 16:12:24 -0700

    5-

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers from 0 to 14
+ */
+
+void more_numbers(void)
+{
+	char i;
+	int r;
+
+	for (r = 0; r < 10; r++)
+	{
+		for (i = '0'; i <= '14'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
+
