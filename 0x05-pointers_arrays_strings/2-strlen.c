cf4a265242ab47fa97622537dd01eaee1e9c158d
ahmed saad <ahmed.saad.kader@gmail.com>
2023-10-16 17:43:07 -0700

    2-

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to return the length of
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	int length;
+
+	length = 0;
+	while (s[length] != "\0")
+		length++;
+
+	return (length);
+}
