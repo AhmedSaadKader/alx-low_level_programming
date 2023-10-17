4cb2e2d0f6ab32124076b23237d47c5f16a95913
ahmed saad <ahmed.saad.kader@gmail.com>
2023-10-16 18:44:04 -0700

    6-

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string 
+ * starting with the first character
+ * @str: string to print it characters
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i = i + 2)
+		_putchar(str[i]);
+}
