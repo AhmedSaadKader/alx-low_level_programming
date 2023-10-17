cc5d40e6676727d67c23e22b827c05d93189e296
ahmed saad <ahmed.saad.kader@gmail.com>
2023-10-16 18:34:15 -0700

    5-

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,4 +7,21 @@
 
 void rev_string(char *s)
 {
+	int length, start, end;
+	char temp;
+
+	length = 0;
+	while (s[length] != '\0')
+		length++;
+
+	end = length - 1;
+	for (start = 0; start < end; start++; end--)
+	{
+		temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+	}
+		
+  
+
   
