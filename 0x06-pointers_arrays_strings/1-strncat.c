354cde95cc096e234e84c4bdd46886d68911f954
AhmedSaadKader <ahmed.saad.kader@gmail.com>
2023-10-19 03:13:03 +0300

    1-

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * strncat - concatenates two strings using at most
+ * n bytes from second string
+ * @dest: string to be concatenated to
+ * @src: string to be concatenated
+ * @n: number of characters from the second string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i; /* length of dest string */
+	int k; /* dest length added to n */
+	int l: /* integer to loop through second string */
+
+	i = 0;
+	while (dest[i] != '\0')
+		i++;
+
+	k = i + n;
+
+	for (l = 0; n <= k; ++n, l++)
+		dest[n] = src[l];
+
+	return (dest);
+}
+	
+	
