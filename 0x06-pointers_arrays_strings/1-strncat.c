dad995377e6a25ac6385a8e087489baf1593de35
AhmedSaadKader <ahmed.saad.kader@gmail.com>
2023-10-19 03:32:04 +0300

    1- betty and logic

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
 /**
- * strncat - concatenates two strings using at most
+ * _strncat - concatenates two strings using at most
  * n bytes from second string
  * @dest: string to be concatenated to
  * @src: string to be concatenated
  * @n: number of characters from the second string
+ *
+ * Return: concatenated string
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
 	int i; /* length of dest string */
+	int x; /* length of src string */
 	int k; /* dest length added to n */
 	int l; /* integer to loop through second string */
 
@@ -18,7 +21,14 @@ char *_strncat(char *dest, char *src, int n)
 	while (dest[i] != '\0')
 		i++;
 
-	k = i + n;
+	x = 0;
+	while (src[x] != '\0')
+		x++;
+
+	if (n > x)
+		k = i + n;
+	else
+		k = i + x;
 
 	for (l = 0; i <= k - 1; ++i, l++)
 		dest[i] = src[l];
