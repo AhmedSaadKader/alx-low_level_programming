e25c794830dbfd4d9b8913cef11e8239dab2fa5a
AhmedSaadKader <ahmed.saad.kader@gmail.com>
2023-10-19 05:21:26 +0300

    7-

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @str: string to be encoded
+ *
+ * Return: encoded string
+ */
+
+char *leet(char *str)
+{
+	int i, l;
+	char c[] = "aAeEoOtTlL";
+	char n[] = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (l = 0; l < 10; l++)
+		{
+			if (str[i] == c[l])
+				s[i] = n[i];
+		}
+	}
+	return (str);
+}
