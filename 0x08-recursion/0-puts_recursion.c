48b5acb276b52a40d3f32ca216c5a756f601ddce
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2023-10-25 22:26:48 +0300

    add recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,9 +9,11 @@ void _puts_recursion(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	i = 0;
+	if (s[i] == '\0')
 	{
-		_putchar(i);
+		_putchar('\n');
 	}
-	_putchar('\n');
+	_putchar(s[i]);
+	_puts_recursion(i++);
 }
