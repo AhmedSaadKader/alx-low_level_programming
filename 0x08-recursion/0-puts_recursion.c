65dd7a53769ab1d04c4e7cfdcf2b814a67d41cd7
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2023-10-25 22:05:24 +0300

    0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: string to print
+ */
+
+void _puts_recursion(char *s)
+{
+	int i;
+x
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
