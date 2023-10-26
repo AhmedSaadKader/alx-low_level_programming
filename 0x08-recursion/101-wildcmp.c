62d533b77fd1eed2357e079b0962a700f24545bb
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2023-10-26 23:25:25 +0200

    101-

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * wildcmp - compares two strings to check if they are identical
+ * @s1: string number one
+ * @s2: string number two
+ *
+ * Return: 1 if strings are identical and 0 if otherwise
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	int a, b;
+
+	a = string_length(s1);
+	b = string_length(s2);
+
+	if (s1[0] == '\0' && s2[0] == '\0')
+		return (1);
+	if (s1[0] == s2[0])
+		return (wildcmp(s1 + 1, s2 + 1));
+	return (0);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,3 +11,4 @@ int find_prime(int a, int n);
 int is_palindrome(char *s);
 int string_length(char *s);
 int compare_letters(char *s, int start, int end);
+int wildcmp(char *s1, char *s2);
