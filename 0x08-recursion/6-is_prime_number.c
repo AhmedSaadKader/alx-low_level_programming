023b6722e6f2f8975a09ce0efa81f0439dc8b3da
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2023-10-26 21:09:57 +0300

    6- logic"

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,28 @@
 #include "main.h"
 
 /**
+ * find_prime - calculates prime recursively
+ * @a: number to divide by
+ * @n: number to check if prime
+ *
+ * Return: 1 if prime number or 0 otherwise
+ */
+
+int find_prime(int a, int n)
+{
+	if (n <= 1)
+		return (0);
+	if (n == 2 || n == 3)
+		return (1);
+	if (n % 2 == 0 || n % 3 == 0)
+		return (0);
+	if (a * a > n)
+		return (1);
+	if (n % a == 0 || n % (i + 2) == 0)
+		return (find_prime(a + 6, n));
+}
+
+/**
  * is_prime_number - checks if prime number
  * @n: number to check
  *
@@ -9,5 +31,8 @@
 
 int is_prime_number(int n)
 {
-	return (n);
+	int x;
+
+	x = find_prime(5, n);
+	return (x);
 }
