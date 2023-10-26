b0b43cd8b06a5e9add824cb24421febad8011cb9
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2023-10-27 00:00:47 +0200

    check if s2 is longer than s1

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -16,5 +16,7 @@ int wildcmp(char *s1, char *s2)
 		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 	if (s1[0] == s2[0])
 		return (wildcmp(s1 + 1, s2 + 1));
+	if (s1[0] == '\0' && s2[j] == '*')
+		return (wildcmp(s1, s2 + 1);
 	return (0);
 }
