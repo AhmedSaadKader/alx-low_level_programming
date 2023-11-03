37e7e52d0f4978e94b7ccab21a0abadbd6756c40
AhmedSaadKader <ahmed.saad.kader@gmail.com>
2023-11-03 12:52:57 +0200

    initialize the array to 0

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,6 +13,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *memalloc;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -20,5 +21,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	memalloc = malloc(nmemb * size);
 	if (memalloc == NULL)
 		return (NULL);
+	for (i = 0; i < (nmemb * size); i++)
+		memalloc[i] = 0;
 	return (memalloc);
 }
