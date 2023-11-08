cac90481bb0e6a362269d07453ebbb2ee5f9bbdc
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2023-11-08 11:06:55 +0200

    2-print_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,7 +14,8 @@ struct dog
 	char *owner;  /* Owner of the dog */
 };
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
 typedef struct dog dog;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
