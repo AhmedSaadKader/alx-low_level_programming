1a3348919d749e3a29b0ea3dfadf23ee8c732b16
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2023-11-09 22:55:44 +0200

    3- working

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,6 +1,8 @@
 #ifndef CALC
 #define CALC
 
+#include <stddef.h>
+
 /**
  * struct op - Struct op
  *
@@ -9,9 +11,11 @@
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+char *op;
+int (*f)(int a, int b);
 } op_t;
+
+int (*get_op_func(char *s))(int, int);
 
 int op_add(int a, int b);
 int op_sub(int a, int b);
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,13 +1,33 @@
 #include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Entry point
- * 
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0
  */
 
 int main(int argc, char **argv)
 {
-	if (argc > 0 && argv)
-		return (0);
+	int (*calculation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	
+	calculation = get_op_func(argv[2]);
+
+	if (calculation == NULL)
+	{
+	  printf("Error\n");
+	  exit(99);
+	}
+
+	printf("%d\n", calculation(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
