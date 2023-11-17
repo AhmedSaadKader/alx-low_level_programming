bb75ec666d20fc33a3bce34f844e6b50370c7575
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2023-11-17 13:05:22 +0200

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+#include "stdio.h"
+
+/**
+ * print_first - prints string before main function executes
+*/
+
+void print_first(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
