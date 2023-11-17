ea43a5fb518b924ed63fd369a9f6737541cea4f8
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2023-11-17 11:00:36 +0200

    2- str len

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,12 +14,15 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
+	int str_len;
 
+	str_len = strlen(str);
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 
 	new->str = strdup(str);
+	new->len = str_len;
 	new->next = *head;
 	*head = new;
 	return (*head);
