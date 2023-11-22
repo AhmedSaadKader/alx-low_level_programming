eae46da86551f0c94dc8b5ca7f6b6ce214ab0ff3
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2023-11-22 16:15:44 +0200

    6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint - remove head node from linked list
+ * @head: linked list to remove head node from
+ *
+ * Return: the n integer of the popped head node
+*/
+
+int pop_listint(listint_t **head)
+{
+	int n;
+	listint_t *current;
+
+	if (*head == NULL)
+		return (0);
+	current = *head;
+	n = current->n;
+	current = current->next;
+	*head = current;
+	return (n);
+}
