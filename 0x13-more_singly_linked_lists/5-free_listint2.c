268da942d67d3b40c00a78303f214714bd9b6e7e
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2023-11-22 15:58:31 +0200

    5- betty and check if head is null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,8 +10,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp, *current;
 
+	if (head == NULL || *head == NULL)
+	{
+		return;
+	}
 	current = *head;
-	while(current != NULL)
+	while (current != NULL)
 	{
 		temp = current;
 		current = current->next;
