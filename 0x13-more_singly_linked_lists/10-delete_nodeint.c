de1886383524cac4c3ae4a98aa7899c673cebd21
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2023-11-23 00:30:34 +0200

    10-delete_nodeint.c update

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,6 +14,9 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int i = 0;
 	listint_t *current, *previous;
 
+	if (*head == NULL)
+		return (-1);
+
 	current = *head;
 	previous = NULL;
 	if (index == 0)
