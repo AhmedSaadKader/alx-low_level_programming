e32d4c397f3e8eb2a258299390bbf43a13c7755b
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2023-11-24 13:40:34 +0200

    2-get_bit.c error handling and betty

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,6 +12,9 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int i;
 	unsigned long int temp;
+
+	if (index > (sizeof(unsigned long int) *8))
+		return (-1);
 	temp = n >> index;
 	i = (temp & 1);
 	return (i);
