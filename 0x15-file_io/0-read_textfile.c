bddac333364b9e02b7498f8cea4da6467eb28450
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2023-11-27 20:36:38 +0200

    0-read_textfile.c update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,13 +14,23 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd, sz, i = 0;
 	char buffer[1024];
 
+	if (filename == NULL)
+		return (0);
 	fd = open(filename, O_RDONLY);
+	if (!fd)
+		return (0);
 	sz = read(fd, buffer, letters);
+	if (sz == -1)
+	{
+		close(fd);
+		return (0);
+	}
 	buffer[sz] = '\0';
 	while (buffer[i] != '\0')
 	{
 		_putchar(buffer[i]);
 		i++;
 	}
+	close(fd);
 	return (sz);
 }
