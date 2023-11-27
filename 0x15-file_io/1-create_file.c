5c2e77511bd2299855c49519944510ae49d666c9
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2023-11-27 22:58:36 +0200

    1-create_file.c error handling

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,9 +18,15 @@ int create_file(const char *filename, char *text_content)
 	cf = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (cf == -1)
 		return (-1);
-	wr = write(cf, text_content, strlen(text_content));
-	if (wr == -1)
-		return (-1);
+	if (text_content != NULL)
+	{
+		wr = write(cf, text_content, strlen(text_content));
+		if (wr == -1)
+		{
+			close(cf);
+			return (-1);
+		}
+	}
 	close(cf);
 	return (1);
 }
