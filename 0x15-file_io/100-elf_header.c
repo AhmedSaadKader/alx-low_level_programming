ca9e6fca8168d1296114c9cae5eef508ccd19ea2
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2023-11-28 20:56:56 +0200

    100-elf_header.c start over

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -25,28 +25,6 @@ void display_elf_header(const char *filename)
 		dprintf(STDERR_FILENO, "Error: Unable to read ELF header");
 		exit(98);
 	}
-	if (elf_header.e_ident[EI_MAG0] != ELFMAG0 ||
-		elf_header.e_ident[EI_MAG1] != ELFMAG1 ||
-		elf_header.e_ident[EI_MAG2] != ELFMAG2 ||
-		elf_header.e_ident[EI_MAG3] != ELFMAG3)
-	{
-		dprintf(STDERR_FILENO, "Error: Not an ELF file");
-		exit(98);
-	}
-	printf("Magic:   %02x %02x %02x %02x\n", elf_header.e_ident[EI_MAG0],
-			elf_header.e_ident[EI_MAG1],
-		   elf_header.e_ident[EI_MAG2], elf_header.e_ident[EI_MAG3]);
-	printf("Class:   %s\n", elf_header.e_ident[EI_CLASS] == ELFCLASS64
-			? "ELF64" : "ELF32");
-	printf("Data:    %s\n", elf_header.e_ident[EI_DATA] == ELFDATA2LSB
-			? "2's complement, little endian" : "Unknown data format");
-	printf("Version: %d\n", elf_header.e_ident[EI_VERSION]);
-	printf("OS/ABI:  %s\n", elf_header.e_ident[EI_OSABI] == ELFOSABI_SYSV
-			? "UNIX System V ABI" : "Other ABI");
-	printf("ABI Version: %d\n", elf_header.e_ident[EI_ABIVERSION]);
-	printf("Type:    %s\n", elf_header.e_type == ET_EXEC
-			? "Executable file" : "Other");
-	printf("Entry point address: %#lx\n", (unsigned long)elf_header.e_entry);
 	close(fd);
 }
 
