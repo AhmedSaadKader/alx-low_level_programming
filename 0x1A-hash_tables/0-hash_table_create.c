713d78481a19654f29838026ad00930c0cac6c53
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-02-01 22:09:17 +0200

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,12 +12,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table;
 	unsigned int i;
 
+	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 	{
 		return (NULL);
 	}
-
-	table = malloc(sizeof(hash_table_t));
 	table->size = size;
 	table->array = malloc(size * sizeof(hash_node_t));
 	if (table->array == NULL)
