bad227ef6eb51bbb5bfa9e597a982daf68efbccd
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-02-01 22:08:23 +0200

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,13 +10,25 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
+	unsigned int i;
+
+	if (table == NULL)
+	{
+		return (NULL);
+	}
 
 	table = malloc(sizeof(hash_table_t));
-	if (table == NULL)
-		return (NULL);
-
 	table->size = size;
-	table->array = calloc(table->size, sizeof(hash_node_t));
+	table->array = malloc(size * sizeof(hash_node_t));
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
 
+	for (i = 0; i < table->size; i++)
+	{
+		table->array[i] = NULL;
+	}
 	return (table);
 }
