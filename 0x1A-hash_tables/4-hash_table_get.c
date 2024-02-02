5589d6af027b7a641add697fa52ba9c2ac7c2fb5
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-02-02 15:01:35 +0200

    4-hash_table_get.c

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: the hash table
+ * @key: the key to retrieve value of
+ *
+ * Return: the value
+*/
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+
+	index = key_index((const unsigned char *)key, ht->size);
+	if (!ht->array[index])
+		return (NULL);
+	return (ht->array[index]->value);
+}
