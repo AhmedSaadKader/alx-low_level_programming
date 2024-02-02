63b41afb963a63d14850bade896672b66cbce259
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-02-02 16:00:47 +0200

    5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -8,8 +8,7 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i = 0, item_count = 0;
-
-
+	hash_node_t *current_node;
 
 	if (ht == NULL || ht->size == 0 || ht->array == NULL)
 		printf("{}\n");
@@ -17,12 +16,14 @@ void hash_table_print(const hash_table_t *ht)
 		printf("{");
 	while (i < ht->size)
 	{
-		if (ht->array[i])
+		current_node = ht->array[i];
+		while (current_node != NULL)
 		{
 			if (item_count > 0)
 				printf(", ");
-			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+			printf("'%s': '%s'", current_node->key, current_node->value);
 			item_count++;
+			current_node = current_node->next;
 		}
 		i++;
 	}
