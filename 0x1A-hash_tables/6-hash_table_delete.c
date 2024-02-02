5d04bfefa5df2604dbba79a1ea1f47f7d859e616
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-02-02 16:21:14 +0200

    6-hash_table_delete.c

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: the hash table
+*/
+
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *current_node, *next_node;
+	unsigned long int i = 0;
+
+	while (i < ht->size)
+	{
+		current_node = ht->array[i];
+		while (current_node != NULL)
+		{
+			next_node = current_node->next;
+			free(current_node->key);
+			free(current_node->value);
+			free(current_node);
+			current_node->next = NULL;
+			current_node = next_node;
+		}
+		i++;
+	}
+	free(ht->array);
+	free(ht);
+}
