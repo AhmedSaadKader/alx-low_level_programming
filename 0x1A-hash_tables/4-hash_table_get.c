125edef545d0daac8563d18156b26e69919709ce
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-02-02 15:06:49 +0200

    4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,13 +10,19 @@
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	hash_node_t *current_node;
 	unsigned long int index;
 
 	if (ht == NULL || ht->array == NULL || key == NULL ||
 		ht->size == 0)
 		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
-	if (!ht->array[index])
+	current_node = ht->array[index];
+	if (!current_node)
 		return (NULL);
-	return (ht->array[index]->value);
+	while (current_node->next != NULL)
+	{
+		current_node = current_node->next;
+	}
+	return (current_node->value);
 }
