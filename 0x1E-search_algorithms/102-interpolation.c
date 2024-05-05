b2f6f3699f03985df01fe23aec8252a5d9957108
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-05-05 23:18:27 +0300

    102-interpolation

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -12,30 +12,52 @@
 
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t pos, low, high;
+    if (array == NULL || size == 0)
+        return (-1);
 
-	low = 0;
-	high = size - 1;
+    return interpolation_search_recursive(array, 0, size, value);
+}
 
-	if (array == NULL || size == 0)
+/**
+ * interpolation_search_recursive - searches for a value in a sorted array of
+ * integers using the interpolation search algorithm
+ * @array: pointer to the first element of the array
+ * @low: starting point of the search
+ * @size: number of elements in array
+ * @value: the value to search for
+ *
+ * Return: the index where the value is located,
+ * -1 if value is not present in array of if array is NULL
+*/
+
+int interpolation_search_recursive(
+	int *array, size_t low, size_t size, int value)
+{
+	size_t pos, high;
+
+	high = low + size - 1;
+	pos = low + ((
+			(high - low) / (array[high] - array[low])) * (value - array[low]));
+
+	if (size == 0)
 		return (-1);
 
-	while (low <= high)
+	if (pos >= size || pos < low || pos > high)
 	{
-		pos = low + ((
-			(high - low) / (array[high] - array[low])) * (value - array[low]));
-		if (pos >= size)
-		{
-			printf("Value checked array[%ld] is out of range\n", pos);
-			return (-1);
-		}
-		printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
-		if (value < array[pos])
-			high = pos - 1;
-		else if (value > array[pos])
-			low = pos + 1;
-		else
-			return (pos);
+		printf("Value checked array[%ld] is out of range\n", pos);
+		return (-1);
 	}
-	return (-1);
+	printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
+
+	if (array[pos] == value)
+		return (pos);
+
+	if (array[pos] < value)
+		return (
+			interpolation_search_recursive(
+				array, pos + 1, size - (pos - low + 1), value)
+			);
+	else
+		return (interpolation_search_recursive(
+			array, low, pos - low, value));
 }
---
+++ 0x1E-search_algorithms/103-exponential.c
@@ -0,0 +1,16 @@
+#include "search_algos.h"
+
+/**
+ * exponential_search - searches for a value in a sorted array of integers
+ * using the Exponential search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: the number of elements of the array
+ * @value: the value to search for
+ *
+ * Return: first index where value is located, otherwise -1
+*/
+
+int exponential_search(int *array, size_t size, int value)
+{
+	
+}
